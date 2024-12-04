5cc180f0e4528be4fb47ae81e7eb1ffdbe879da5
Edoardo <edoardoderosaa@gmail.com>
2024-12-04 09:12:56 +0100

    Aggiunta commenti

--- Esercizimetodi/src/main/Metodi.java
+++ Esercizimetodi/src/main/Metodi.java
@@ -14,22 +14,38 @@ import java.util.ArrayList;
 public class Metodi {
 
 	public static void main(String[] args) {
+
+		// Creazione di un arraylist di numeri
+
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
 		numbers.add(10);
 		numbers.add(20);
 		numbers.add(30);
 		numbers.add(40);
 		numbers.add(50);
+
+		// Assegna il metodo ad una variabile e stampa
+
 		int avg = calcolaMedia(numbers);
 		System.out.print(avg);
 	}
 
+	// Creazione metodo
+
 	static int calcolaMedia(ArrayList<Integer> lista) {
+
+		// Variabile per somma e lunghezza
+
 		int sum = 0;
 		int size = lista.size();
+
+		// Ciclo per fare la somma dei numeri
+
 		for (int i = 0; i < lista.size(); i++) {
 			sum += lista.get(i);
 		}
+
+		// Ritorna il calcolo della media
 
 		return sum / size;
 	}
